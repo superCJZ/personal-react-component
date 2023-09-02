f4ac367fa1a816ab71191552ad5920fc385da402
cjz <614942063@qq.com>
2023-09-02 18:53:42 +0800

    update transition-input

--- transition-input/dist/transition-input.d.ts
+++ transition-input/dist/transition-input.d.ts
@@ -1,11 +1,14 @@
-import { ChangeEventHandler } from 'react';
-import './transition-input.scss';
+/// <reference types="react" />
+import './index.scss';
 interface Props {
     placeholder?: string;
     btnStyle?: string;
     inputStyle?: string;
-    onChangeFn?: ChangeEventHandler<HTMLInputElement>;
+    onChangeFn?: Function;
     onClickFn?: Function;
+    onFocus?: React.FocusEventHandler<HTMLInputElement>;
+    value?: string;
+    width?: string;
 }
-declare function TransitionInput({ placeholder, btnStyle, inputStyle, onChangeFn, onClickFn }: Props): import("react/jsx-runtime").JSX.Element;
+declare function TransitionInput({ width, placeholder, btnStyle, inputStyle, onChangeFn, onClickFn, onFocus, value }: Props): import("react/jsx-runtime").JSX.Element;
 export default TransitionInput;
